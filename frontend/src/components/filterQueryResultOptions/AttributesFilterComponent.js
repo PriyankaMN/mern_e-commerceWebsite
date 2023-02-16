f8f36cbaf87c3e31e391a756cae200b0add3c787
PriyankaMN <priyankamn0507@gmail.com>
2023-02-16 14:14:48 +0530

    commit

--- frontend/src/components/filterQueryResultOptions/AttributesFilterComponent.js
+++ frontend/src/components/filterQueryResultOptions/AttributesFilterComponent.js
@@ -3,12 +3,19 @@ import { Form } from "react-bootstrap";
 const AttributesFilterComponent = () => {
   return (
     <>
-      <Form.Label>Color</Form.Label>
-      <Form.Check 
-        type="checkbox"
-        id="default-checkbox"
-        label="green"
-      />
+      {[
+        { Ram: ["1 TB", "2 TB", "4 TB"] },
+        { Color: ["red", "blue", "green", "black"] },
+      ].map((item, idx) => (
+        <div key={idx} className="mb-3">
+          <Form.Label>
+            <b>{Object.keys(item)}</b>
+          </Form.Label>
+          {item[Object.keys(item)].map((i, idx) => (
+            <Form.Check key={idx} type="checkbox" label={i} />
+          ))}
+        </div>
+      ))}
     </>
   );
 };
