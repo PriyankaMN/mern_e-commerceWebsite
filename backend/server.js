9657deff85c02e0970fa2ac14c81f0afe97fe1eb
priyankamn <priyankamn0705@gmail.com>
2023-02-21 20:25:59 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const apiRoutes = require("./routes/apiRoutes");
+const apiRoutes = require("./routes/apiRoutes")
 
-app.get("/", (req, res) => {
-  res.json({ message: "API running..." });
-});
+app.get('/', (req, res) => {
+    console.log("synchronous code")
+    throw new Error("some error occured")
+    res.json({message: "API running..."})
+})
 
-app.use("/api", apiRoutes);
-// app.get('/api/products', (req,res) => {
-//     res.send("Handling product routes.")
-// })
+app.get('/a', (req,res,next) => {
+    setTimeout(() => {
+        console.log("asynchronouse code");
+        // throw new Error("some error occured")
+        next(new Error("some error occured"))
+    },1000)
+    // res.send("Hello World!")
+})
+
+app.use('/api', apiRoutes)
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
