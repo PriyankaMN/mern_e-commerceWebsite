8b42f8963e2a105e90e3edf25efc5cafbdf689ef
priyankamn <priyankamn0705@gmail.com>
2023-02-21 20:06:00 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,16 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-const apiRoutes = require("./routes/apiRoutes");
+const apiRoutes = require("./routes/apiRoutes")
 
-app.get("/", (req, res) => {
-  res.json({ message: "API running..." });
-});
+app.get('/', (req, res) => {
+    res.json({message: "API running..."})
+})
 
-app.use("/api", apiRoutes);
-// app.get('/api/products', (req,res) => {
-//     res.send("Handling product routes.")
-// })
+app.use('/api', apiRoutes)
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
