29b97ceeb959f11c5f5b937bdfbae27f9ef1c20a
priyankamn <priyankamn0705@gmail.com>
2023-05-21 00:47:11 +0530

    commit

--- frontend/backend/controllers/productController.js
+++
@@ -1,7 +0,0 @@
-const Product = require("../models/ProductModel")
-
-const getProducts = (req, res) => {
-    Product.create({name: "Panasonic"})
-    res.send("Handling product routes, e.g. search for products")
-}
-module.exports = getProducts
