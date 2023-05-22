b5856f102f130366c55f82bbe1e70ab2dbb9031f
priyankamn <priyankamn0705@gmail.com>
2023-05-22 14:51:57 +0530

    commit

--- frontend/src/pages/ProductListPage.js
+++ frontend/src/pages/ProductListPage.js
@@ -6,11 +6,7 @@ import PriceFilterComponent from "../components/filterQueryResultOptions/PriceFi
 import RatingFilterComponent from "../components/filterQueryResultOptions/RatingFilterComponent";
 import CategoryFilterComponent from "../components/filterQueryResultOptions/CategoryFilterComponent";
 import AttributesFilterComponent from "../components/filterQueryResultOptions/AttributesFilterComponent";
-
-import axios from "axios";
-
 const ProductListPage = () => {
-    axios.get("/api/products").then((res) => console.log(res));
   return (
     <Container fluid>
       <Row>
@@ -33,7 +29,7 @@ const ProductListPage = () => {
               <AttributesFilterComponent />
             </ListGroup.Item>
             <ListGroup.Item>
-              <Button variant="primary">Filter</Button>{" "}
+              <Button variant="primary">Filter</Button>
               <Button variant="danger">Reset filters</Button>
             </ListGroup.Item>
           </ListGroup>
